d90e357806a584f988ae91fe2d73accc3aabbf0d
thethomasboyer <warbeaver@pm.me>
2026-05-15 21:20:09 +0000

    Deploying to gh-pages from @ thethomasboyer/thethomasboyer.github.io@9cd27347924847ba323f7d904c0de73f025a984c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,37 +23,85 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{
-        id: 'social-github',
-        title: 'GitHub',
+        },{id: "post-on-the-reproducibility-of-ml-experiments",
+      
+        title: "On the Reproducibility of ML experiments",
+      
+      description: "Sometimes when discussing or reading about reproducibility in ML experiments you would stumble upon mentions of random seeds (e.g. here or there). Then the discussion usually revolves around the idea that you should set these as fixed, hard-coded values, otherwise other people might not be able to exactly reproduce your experiments, and that this has to do with Reproducibility. Even the official PyTorch documentation page titled “Reproducibility”The webpage name is actually &quot;randomness&quot;; says something about the conflation of both ideas, doesn&#39;t it?! starts with a “Controlling sources of randomness” section where you are somehow invited to torch.manual_seed(0), random.seed(0), np.random.seed(0). What...",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2026/on-reproducibility/";
+        
+      },
+    },{
+      id: "publication-bourou2024phendiff",
+      title: "PhenDiff: Revealing Subtle Phenotypes with Diffusion Models in Real Images",
+      description: "Anis Bourou, Thomas Boyer, Marzieh Gheisari, Kévin Daupin, Véronique Dubreuil, Aurélie De Thonel, Valérie Mezger, Auguste Genovesio - MICCAI, 2024",
+      section: "Publications",
+      handler: () => {
+        window.location.href = "/publications/#bourou2024phendiff";
+      },
+    },{
+      id: "publication-myara2026xfactors",
+      title: "XFACTORS: Disentangled Information Bottleneck via Contrastive Supervision",
+      description: "Alexandre Myara, Nicolas Bourriez, Thomas Boyer, Thomas Lemercier, Ihab Bendidi, Auguste Genovesio - arXiv, 2026",
+      section: "Publications",
+      handler: () => {
+        window.location.href = "/publications/#myara2026xfactors";
+      },
+    },{
+      id: "publication-gravier2026mmtsbm",
+      title: "Multi-marginal temporal Schrödinger Bridge Matching from unpaired data",
+      description: "Thomas Gravier, Thomas Boyer, Auguste Genovesio - ICML, 2026",
+      section: "Publications",
+      handler: () => {
+        window.location.href = "/publications/#gravier2026mmtsbm";
+      },
+    },{
+      id: "publication-bourou2025diffex",
+      title: "DiffEx: Explaining a Classifier with Diffusion Models to Identify Microscopic Cellular Variations",
+      description: "Anis Bourou, Saranga Kingkor Mahanta, Thomas Boyer, Valérie Mezger, Auguste Genovesio - arXiv, 2025",
+      section: "Publications",
+      handler: () => {
+        window.location.href = "/publications/#bourou2025diffex";
+      },
+    },{
+      id: "publication-bourou2025latentdiffusion",
+      title: "Revealing Subtle Phenotypes in Small Microscopy Datasets Using Latent Diffusion Models",
+      description: "Anis Bourou, Biel Castaño Segade, Thomas Boyer, Valérie Mezger, Auguste Genovesio - CVPR CVDD, 2025",
+      section: "Publications",
+      handler: () => {
+        window.location.href = "/publications/#bourou2025latentdiffusion";
+      },
+    },{
+        id: 'social-email',
+        title: 'email',
+        description: 'thomasboyer@protonmail.ch',
         section: 'Socials',
         handler: () => {
-          window.open("https://github.com/thethomasboyer", "_blank");
+          window.open("mailto:%74%68%6F%6D%61%73%62%6F%79%65%72@%70%72%6F%74%6F%6E%6D%61%69%6C.%63%68", "_blank");
         },
       },{
         id: 'social-x',
         title: 'X',
+        description: 'TheThomasBoyer',
         section: 'Socials',
         handler: () => {
           window.open("https://twitter.com/TheThomasBoyer", "_blank");
         },
       },{
-        id: 'social-bluesky',
-        title: 'Bluesky',
+        id: 'social-github',
+        title: 'GitHub',
+        description: 'thethomasboyer',
         section: 'Socials',
         handler: () => {
-          window.open("https://bsky.app/profile/thethomasboyer.bsky.social", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/thomas-boyer-fr", "_blank");
+          window.open("https://github.com/thethomasboyer", "_blank");
         },
       },{
         id: 'social-orcid',
         title: 'ORCID',
+        description: '0009-0000-8554-4864',
         section: 'Socials',
         handler: () => {
           window.open("https://orcid.org/0009-0000-8554-4864", "_blank");
@@ -61,20 +109,15 @@ ninja.data = [{
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
+        description: 'eqheP7MAAAAJ',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=eqheP7MAAAAJ&hl", "_blank");
-        },
-      },{
-        id: 'social-semanticscholar',
-        title: 'Semantic Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.semanticscholar.org/author/2273953033", "_blank");
+          window.open("https://scholar.google.com/citations?user=eqheP7MAAAAJ", "_blank");
         },
       },{
         id: 'social-rss',
         title: 'RSS Feed',
+        description: 'true',
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
